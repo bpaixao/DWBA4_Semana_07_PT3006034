ed5832ac5ca935474e49863e8b0045d72176ea52
Beatriz Paixão <bpaixao@live.com>
2020-12-24 02:40:06 +0000

    update-SlideShow

--- js/script.js
+++ js/script.js
@@ -8,23 +8,21 @@ function plusSlides(n) {
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n;
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i; 
-  var slides = document.getElementsByClassName("mySlides1", "mySlides2");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 1; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
