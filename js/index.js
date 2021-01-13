426770b672eb6b4f93f68129f342c0f9b126cb2d
Beatriz Paixão <bpaixao@live.com>
2021-01-13 21:05:49 +0000

    up-date

--- js/index.js
+++ js/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const myelement =  (
+ReactDOM.render(<p> O <b>Instituto Federal de Educação, Ciência e Tecnologia de São Paulo</b> – é uma autarquia federal de ensino.</p>, document.getElementById('app1'));
 
-  <div>
-  <p>Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.</p>
-  
-  <p>Durante sua história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.</p>
+ReactDOM.render(<p>Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.</p>, document.getElementById('app2'));
 
-  <p>Com a mudança, o Instituto Federal de São Paulo passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.</p>
-  
-  <p>Além dos cursos presenciais, o Instituto Federal de São Paulo oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).</p>
+ReactDOM.render(<p>Durante sua história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.</p>, document.getElementById('app3'));
 
-  <p>O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuídas pelo estado de São Paulo.</p>
+ReactDOM.render(<p>Com a mudança, o Instituto Federal de São Paulo passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.</p>, document.getElementById('app4'));
 
-  <p>Localizado em <B>Rua Pedro Vicente, 625 - Canindé - São Paulo - SP - Brasil - Cep: 01109-010</B></p> 
+ReactDOM.render(<p>Além dos cursos presenciais, o Instituto Federal de São Paulo oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).</p>, document.getElementById('app5'));
 
-</div> );
+ReactDOM.render(<p>O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuídas pelo estado de São Paulo.</p>, document.getElementById('app6'));
 
-ReactDOM.render(myelement, document.getElementById('root'));
+ReactDOM.render(<p>Localizado em <b>Rua Pedro Vicente, 625 - Canindé - São Paulo - SP - Brasil - Cep: 01109-010<b><p>, document.getElementById('app7'));
+
+
+
+
